629c7641b96ca0c74651528acb496a80a56953f7
And-Rom <you@example.com>
2023-02-26 17:35:27 +0300

    Added Task HW3_4

--- Test/seminars/third/tdd/UserTest.java
+++ Test/seminars/third/tdd/UserTest.java
@@ -1,10 +1,7 @@
 package seminars.third.tdd;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
