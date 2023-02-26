ae8b8fd95d2261332ff1ae12f2dfa541d2943040
And-Rom <you@example.com>
2023-02-26 10:00:49 +0300

    Added Lesson task 3_2

--- Test/seminars/third/coverage/SomeServiceTest.java
+++ Test/seminars/third/coverage/SomeServiceTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class SomeServiceTest {
     private SomeService someService;
@@ -32,6 +34,23 @@ class SomeServiceTest {
     @Test
     void testReturnEcho() {
         assertThat(someService.fizzBuzz(14)).isEqualTo("" + 14);
+    }
+
+    @Test
+    void testFirstSixElement() {
+        assertTrue(someService.firstLast6(new int[]{6, 1, 2, 3}));
+
+    }
+
+    @Test
+    void testLastSixElement() {
+        assertTrue(someService.firstLast6(new int[]{1, 2, 3, 6}));
+
+    }
+
+    @Test
+    void testFirstLastNoSixElement() {
+        assertFalse(someService.firstLast6(new int[]{1, 2, 3}));
     }
 
 }
